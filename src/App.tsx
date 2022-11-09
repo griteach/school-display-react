e3dc0f56128935abfdc38c6f32c3f630fa913ced
griteach <griteach22@gmail.com>
2022-11-09 12:36:23 +0900

    api update...

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { getDust, getFcst, IGetFcstResult, IGetDustResult } from "./api";
+import {
+  getDust,
+  getWeatherFcst,
+  IGetFcstResult,
+  IGetDustResult,
+  getDustFcst,
+  IGetDustFcstResult,
+} from "./api";
 
 function App() {
   const { data: fcstData, isLoading: fcstIsLoading } = useQuery<IGetFcstResult>(
     "fcst",
-    getFcst
+    getWeatherFcst
   );
   const { data: dustData, isLoading: dustIsLoading } = useQuery<IGetDustResult>(
     "dust",
     getDust
   );
+  const { data: dustFcstData, isLoading: dustFcstIsLoading } =
+    useQuery<IGetDustFcstResult>("dustFcst", getDustFcst);
   console.log(fcstData, fcstIsLoading);
   console.log(dustData, dustIsLoading);
+  console.log(dustFcstData, dustFcstIsLoading);
   return (
     <>
       <div>{fcstData?.response.body.items.item[0].category}</div>;
       <div>{dustData?.response.body.items[0].pm10Value}</div>;
+      <div>{dustFcstData?.response.body.items[0].informOverall}</div>;
     </>
   );
 }
--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,18 @@
 const API_KEY =
   "l72zwz6RqrexXr8a4wslQsw%2Bx0zTGnE5R1sSf26aPRPOQytFjk3AkCOTfssOo1TQ8xQoimJbfkfYL6YZr%2FssIw%3D%3D";
-const WEATHER_PATH =
+
+//일기예보 기본 패쓰
+const WEATHER_PATH_BASIC =
   "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
 
-const DUST_PATH =
-  "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+//미세먼지 기본 패쓰
+const DUST_PATH_BASIC = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc";
 
-//?&returnType=json&numOfRows=100&pageNo=1&stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC&dataTerm=DAILY&ver=1.0
+//측정소별 실시간 미세먼지 데이터
+const DUST_URL = "/getCtprvnRltmMesureDnsty";
+
+//미세먼지 예측 예보
+const Frcst_URL = "/getMinuDustFrcstDspth";
 
 //일기예보 interface
 interface IFcst {
@@ -34,6 +40,16 @@ interface IDust {
   stationName: string;
 }
 
+interface IDustFcst {
+  dataTime: string;
+
+  informCause: string; //"○ [미세먼지] 원활한 대기 확산으로 대기 상태가 청정할 것으로 예상됩니다."
+  informCode: string; // "PM10" or "PM25" or "O3"
+  informData: string; //"2022-10-10" //예측을 한 날짜
+  informGrade: string; // "서울 : 좋음,제주 : 좋음,전남 : 좋음,전북 : 좋음,광주 : 좋음,경남 : 좋음,경북 : 좋음,울산 : 좋음,대구 : 좋음,부산 : 좋음,충남 : 좋음,충북 : 좋음,세종 : 좋음,대전 : 좋음,영동 : 좋음,영서 : 좋음,경기남부 : 좋음,경기북부 : 좋음,인천 : 좋음"
+  informOverall: string; //"○ [미세먼지] 전 권역이 '좋음'으로 예상됩니다."
+}
+
 export interface IGetFcstResult {
   response: {
     body: {
@@ -52,16 +68,30 @@ export interface IGetDustResult {
   };
 }
 
-export function getFcst() {
+export interface IGetDustFcstResult {
+  response: {
+    body: {
+      items: IDustFcst[];
+    };
+  };
+}
+
+export function getWeatherFcst() {
   return fetch(
-    `${WEATHER_PATH}?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20221108&base_time=1900&nx=55&ny=127`
+    `${WEATHER_PATH_BASIC}?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20221108&base_time=1900&nx=55&ny=127`
   ).then((response) => response.json());
 }
 
 export function getDust() {
   return fetch(
-    `${DUST_PATH}?serviceKey=${API_KEY}&numOfRows=100&returnType=json&ver=1.3&sidoName=${encodeURIComponent(
+    `${DUST_PATH_BASIC}${DUST_URL}?serviceKey=${API_KEY}&numOfRows=100&returnType=json&ver=1.3&sidoName=${encodeURIComponent(
       "강원"
     )}`
+  ).then((response) => response.json());
+}
+
+export function getDustFcst() {
+  return fetch(
+    `${DUST_PATH_BASIC}${Frcst_URL}?serviceKey=${API_KEY}&numOfRows=100&returnType=json&searchDate=2022-10-10&InformCode=PM10`
   ).then((response) => response.json());
 }
