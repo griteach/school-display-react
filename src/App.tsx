8932aba8b06b36f9ff6834e63a818f27c6c492aa
griteach <griteach22@gmail.com>
2022-11-09 12:54:18 +0900

    fix...

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,39 @@ function App() {
   console.log(dustFcstData, dustFcstIsLoading);
   return (
     <>
-      <div>{fcstData?.response.body.items.item[0].category}</div>;
-      <div>{dustData?.response.body.items[0].pm10Value}</div>;
-      <div>{dustFcstData?.response.body.items[0].informOverall}</div>;
+      <div>
+        <h1>FcstData</h1>
+        <p>
+          <p>{fcstData?.response.body.items.item[0].category}</p>
+          <p>{fcstData?.response.body.items.item[0].baseTime}</p>
+          <p>{fcstData?.response.body.items.item[0].nx}</p>
+          <p>{fcstData?.response.body.items.item[0].ny}</p>
+          <p>{fcstData?.response.body.items.item[0].obsrValue}</p>
+        </p>
+      </div>
+      ;
+      <div>
+        <h1>DustData</h1>
+        <p>
+          <p>{dustData?.response.body.items[0]?.dataTime}</p>
+          <p>{dustData?.response.body.items[0].pm10Grade}</p>
+          <p>{dustData?.response.body.items[0].pm10Grade1h}</p>
+          <p>{dustData?.response.body.items[0].pm10Value}</p>
+          <p>{dustData?.response.body.items[0].pm10Value24}</p>
+          <p> {dustData?.response.body.items[0].pm25Grade}</p>
+          <p>{dustData?.response.body.items[0].pm25Grade1h}</p>
+          <p>{dustData?.response.body.items[0].pm25Value}</p>
+          <p>{dustData?.response.body.items[0].pm25Value24}</p>
+          <p>{dustData?.response.body.items[0].khaiGrade}</p>
+          <p>{dustData?.response.body.items[0].khaiValue}</p>
+        </p>
+      </div>
+      ;
+      <div>
+        <h1>DustFcstData</h1>
+        <p>{dustFcstData?.response.body.items[0].informOverall}</p>
+      </div>
+      ;
     </>
   );
 }
